fbc7dc04c75355ec301c28cc554856760872071d
Vovere/Rokas <48134959+voovere@users.noreply.github.com>
2023-12-04 21:35:50 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
 const connection = mysql.createConnection({
     host: 'db',
