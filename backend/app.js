b4f99c72a320662dda96ee727f86996fa44db3ee
Vovere/Rokas <48134959+voovere@users.noreply.github.com>
2023-12-05 20:19:01 +0200

    Revert "a"
    This reverts commit 5ecf58f62c6efaafa72f5ffe533774b142a2b877.

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: ['http://vhost1/', 'http://website.localhost/']
+    origin: '*'
 }));
 
 const connection = mysql.createConnection({
